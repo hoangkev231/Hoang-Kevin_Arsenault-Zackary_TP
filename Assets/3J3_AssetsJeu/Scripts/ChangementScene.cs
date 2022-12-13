586de1c10aba264e39dceb05899b84c4f50b1c17
Kevin Hoang <hoangkev231@gmail.com>
2022-12-13 16:15:38 -0500

    (Zachary) Change scène RV, bouton reset voiture

--- Assets/3J3_AssetsJeu/Scripts/ChangementScene.cs
+++ Assets/3J3_AssetsJeu/Scripts/ChangementScene.cs
@@ -2,28 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.XR;
+using System.Linq;
 public class ChangementScene : MonoBehaviour
 {
-    // Déclaration des propriétés
-    public string nom;
 
+    //InputVr
+    [SerializeField]
+    private XRNode xrNode = XRNode.LeftHand;
+    private List<InputDevice> devices = new List<InputDevice>();
+    private InputDevice device;
+    //to avoid repeat readings
+    public static bool primaryButtonIsPressed;
+    public string nom;
+    void GetDevice()
+    {
+        InputDevices.GetDevicesAtXRNode(xrNode, devices);
+        device = devices.FirstOrDefault();    }
+    private void OnEnable()
+    {
+
+        if (!device.isValid)
+        {
+            GetDevice();
+        }
+
+    }
     void Start()
     {
-
     }
-
     void Update()
     {
+        if (!device.isValid)
+        {
+            GetDevice();
+        }
+        bool primaryButtonValue = false;
+        InputFeatureUsage<bool> primaryButtonUsage = CommonUsages.primaryButton;
+
         if (Input.GetKeyDown("a"))
         {
+
             SceneManager.LoadScene("SceneJeu");
         }
-    }
+        if (device.TryGetFeatureValue(primaryButtonUsage, out primaryButtonValue) && primaryButtonValue && !primaryButtonIsPressed)
+        {
+            primaryButtonIsPressed = true;
+            SceneManager.LoadScene("SceneJeu");
+        }
+        else if (!primaryButtonValue && primaryButtonIsPressed)
+        {
+            primaryButtonIsPressed = false;
 
-    public void CommencerJeu(string sceneJeu)
-    {
-        // Changer la scène
-        SceneManager.LoadScene("SceneJeu");
+        }
     }
 }
