3d8728566c56832869afc3df5153d41bf1ed52fe
Zachary Arsenault <zaki55515@hotmail.ca>
2022-12-13 11:24:30 -0500

    Modifier changement scene Vr

--- Assets/3J3_AssetsJeu/Scripts/ChangementScene.cs
+++ Assets/3J3_AssetsJeu/Scripts/ChangementScene.cs
@@ -1,59 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.XR;
+using System.Linq;
 public class ChangementScene : MonoBehaviour
 {
-    // Déclaration des propriétés
-    public GameObject musique;
-    public GameObject boutonCommencer;
-    public InputField monInputField;
-    public Text nomUI;
-    public string nom;
-    public static bool ddolDejaFait = false;
 
+    //InputVr
+    [SerializeField]
+    private XRNode xrNode = XRNode.LeftHand;
+    private List<InputDevice> devices = new List<InputDevice>();
+    private InputDevice device;
+    //to avoid repeat readings
+    public static bool primaryButtonIsPressed;
+    public string nom;
+    void GetDevice()
+    {
+        InputDevices.GetDevicesAtXRNode(xrNode, devices);
+        device = devices.FirstOrDefault();    }
+    private void OnEnable()
+    {
+
+        if (!device.isValid)
+        {
+            GetDevice();
+        }
+
+    }
     void Start()
     {
-        boutonCommencer.SetActive(false);
-
-        // Garder la musique
-        if (ddolDejaFait == false)
-        {
-            DontDestroyOnLoad(musique);
-            ddolDejaFait = true;
-        }
-        else
-        {
-            Destroy(musique);
-        }
     }
-
     void Update()
     {
-        // Charger constamment le nom saisi
-        nom = (PlayerPrefs.GetString("Nom"));
-    }
-    public void GestionTexteEntre()
-    {
-        string texteSaisi = monInputField.text;
-
-        // Saisir le nom et interdir l'accès pour jouer sans nom
-        if (texteSaisi != "")
+        if (!device.isValid)
         {
-            PlayerPrefs.SetString("Nom", texteSaisi);
-            boutonCommencer.SetActive(true);
+            GetDevice();
         }
-        else
-        {
-            texteSaisi = "";
-        }
-    }
+        bool primaryButtonValue = false;
+        InputFeatureUsage<bool> primaryButtonUsage = CommonUsages.primaryButton;
 
-    public void CommencerJeu(string sceneJeu)
-    {
-        // Changer la scène
-        SceneManager.LoadScene("SceneJeu");
+        if (Input.GetKeyDown("a"))
+        {
+
+            SceneManager.LoadScene("SceneJeu");
+        }
+        if (device.TryGetFeatureValue(primaryButtonUsage, out primaryButtonValue) && primaryButtonValue && !primaryButtonIsPressed)
+        {
+            primaryButtonIsPressed = true;
+            SceneManager.LoadScene("SceneJeu");
+        }
+        else if (!primaryButtonValue && primaryButtonIsPressed)
+        {
+            primaryButtonIsPressed = false;
+
+        }
     }
 }
